39c323ae3d9c23168ce05cc9f2a848557b9f6599
Marko Vejnovic <contact@markovejnovic.com>
2023-05-16 22:56:02 -0500

    Attempted to output sound

--- src/mod_fpgaudio.sv
+++ src/mod_fpgaudio.sv
@@ -3,7 +3,11 @@ module mod_fpgaudio
   , output logic [0:6] o_lcd7_1
   , output logic o_led_1hz
 
-  , input logic i_midi_data
+  , output logic o_audio_dacdat
+  , inout logic i_audio_bclk
+  , inout logic i_audio_adclrck
+  , inout logic i_audio_daclrck
+  , input logic i_audio_adcdat
 
   , input logic i_clk
   , input logic i_nrst
@@ -18,25 +22,41 @@ module mod_fpgaudio
   // Blink LED at 1Hz for a sanity check.
   assign o_led_1hz = clk_1hz;
 
-  logic [7:0] midi_decoded;
-  logic midi_valid;
-  midi_receiver u_midi_receiver
-    ( .clk(i_clk)
-    , .reset(~i_nrst)
-    , .din(i_midi_data)
-    , .dout(midi_decoded)
-    , .valid(midi_valid)
-    );
+  logic [23:0] write_data = 0;
+  logic audio_in_ready;
+  logic audio_next_sample_ready;
+  logic [23:0] read_data_left;
+  logic [23:0] read_data_right;
 
-  logic [7:0] midi_latched = 0;
-  always_ff @(posedge i_clk)
-    if (midi_valid) midi_latched <= midi_decoded;
+  audio_codec u_sound_driver
+    ( .CLOCK_50(i_clk)
+    , .reset(~i_nrst)
+    , .read_s(1'b0)
+    , .write_s(1'b1)
+    , .writedata_left(write_data)
+    , .writedata_right(write_data)
+    , .AUD_ADCDAT(i_audio_adcdat)
+    , .AUD_BCLK(i_audio_bclk)
+    , .AUD_ADCLRCK(i_audio_adclrck)
+    , .AUD_DACLRCK(i_audio_daclrck)
+    , .read_ready(audio_in_ready)
+    , .write_ready(audio_next_sample_ready)
+    , .readdata_left(read_data_left)
+    , .readdata_right(readdata_right)
+    , .AUD_DACDAT(o_audio_dacdat)
+    );
+  
+
+  logic [7:0] counter = 0;
+  always_ff @(posedge clk_1hz or negedge i_nrst)
+    counter <= (~i_nrst) ? 0 : counter + 1;
 
   mod_byte_display u_display
     ( .o_lcd_upper_nibble(o_lcd7_1)
     , .o_lcd_lower_nibble(o_lcd7_0)
-    , .i_value(midi_latched)
+    , .i_value(counter)
     , .i_clk(i_clk)
     , .i_nrst(i_nrst)
     );
+
 endmodule
